2591ad206c915673c2a9a39a160d00a98186661d
petrero <petrero@gmail.com>
2020-09-24 16:35:33 +0300

    10.1. Extracting Drag&Drop -- Create a draggable reusable component

---
+++ src/components/AppDragg.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    draggable
+    @dragover.prevent
+    @dragenter.prevent
+    @dragstart.self="onDrag"
+  >
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    transferData: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    onDrag(e){
+      e.dataTransfer.setData('payload', JSON.stringify(this.transferData))
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
